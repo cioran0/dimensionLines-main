f52c6c1c53f03653505825da610a662b0f4ae675
cioran0 <michaelmurdoch0@gmail.com>
2024-10-22 14:08:50 -0400

    minor changes

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const canvas = document.getElementById("canvas");
+var rect = canvas.getBoundingClientRect();
+console.log("Rect ", rect)
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 500, 100);
+let xCan = ctx.getBoundingClientRect().left
+let yCan = ctx.getBoundingClientRect().top
+console.log("xCan", xCan, "yCan", yCan)
+
+//ctx.fillStyle = "green";
+//ctx.fillRect(10, 10, 500, 100);
+
+let path1 = new Path2D();
+path1.rect(10, 10, 100, 100);
+
+let path2 = new Path2D(path1);
+path2.moveTo(220, 60);
+path2.arc(170, 60, 50, 0, 2 * Math.PI);
+
+ctx.stroke(path2);
+
+// Reset the current path
+//ctx.beginPath(); 
+// Staring point (10,45)
+ ctx.moveTo(25,47);
+// End point (180,47)
+ctx.lineTo(180,47);
+// Make the line visible
+ctx.stroke();
+
+canvas.addEventListener ('click', event => {
+    console.log(`x: ${event.screenX}, y: ${event.screenY}`)
+  })
